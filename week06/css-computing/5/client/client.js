4d9460ca4fd61783891b5171d2fa3936c9d2e268
wucong <wucong@lianfan.com>
2020-05-18 21:55:09 +0800

    feat: css computing 第五步-计算选择器与元素匹配

---
+++ week06/css-computing/5/client/client.js
@@ -0,0 +1,61 @@
+const Request =  require('./Request');
+const parser = require('../parser');
+
+void async function() {
+    const request = new Request({
+        method: 'POST',
+        host: '127.0.0.1',
+        port: 8088,
+        headers: {
+            'Y-Foo': 'bar',
+        },
+        body: {
+            name: 'link.w',
+        },
+    });
+    
+    const res = await request.send();
+    const dom = parser.parseHTML(res.body);
+    console.log(dom);
+}();
+
+// const client = net.createConnection({
+//     host: '127.0.0.1',
+//     port: 8088
+// },
+// () => {
+//         // 'connect' listener.
+//         console.log('connected to server!');
+
+//         const request = new Request({
+//             method: 'POST',
+//             host: '127.0.0.1',
+//             port: 8088,
+//             headers: {
+//                 'Y-Foo': 'bar',
+//             },
+//             body: {
+//                 name: 'link.w',
+//             },
+//         })
+//         console.log(request.toString());
+//         client.write(request.toString());
+// //         client.write(
+// //             `POST / HTTP/1.1\r
+// // Host: 127.0.0.1\r
+// // Content-Length: 10\r
+// // Content-Type: application/x-www-form-urlencoded\r
+// // \r
+// // field1=aaa\r\n`
+// // );
+// }
+// );
+
+// client.on('data', (data) => {
+//     console.log(data.toString());
+//     client.end();
+// });
+
+// client.on('end', () => {
+//     console.log('disconnected from server');
+// });
--- week06/css-computing/5/cssFuncs.js
+++ week06/css-computing/5/cssFuncs.js
@@ -9,7 +9,24 @@ function addCssRules(text) {
 }
 
 function match(element, selector) {
-    
+    if (!selector || !element.attributes)
+        return false;
+
+    if (selector.charAt(0) === "#") {
+        const attr = element.attributes.filter(attr => attr.name === "id")[0];
+        if (attr && attr.value === selector.replace("#", ""))
+            return true;
+    } else if (selector.charAt(0) === ".") {
+        const attr = element.attributes.filter(attr => attr.name === "class")[0];
+        if (attr && attr.value === selector.replace(".", ""))
+            return true;
+    } else {
+        if (element.tagName === selector) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 function computeCSS(element, stack) {
