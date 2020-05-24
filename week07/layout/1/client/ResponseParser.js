fc37ccd9f09e6d1d8c6f5e512826803310318d91
wucong <wucong@lianfan.com>
2020-05-24 14:39:42 +0800

    feat: layout第一步-抽象方向为主轴交叉轴

---
+++ week07/layout/1/client/ResponseParser.js
@@ -0,0 +1,95 @@
+const TrunkedBodyParser = require('./TrunkedBodyParser');
+
+class ResponseParser {
+    constructor() {
+        this.WAITING_STATUS_LINE = 0;
+        this.WAITING_STATUS_LINE_END = 1;
+        this.WAITING_HEADER_NAME = 2;
+        this.WAITING_HEADER_SPACE = 3;
+        this.WAITING_HEADER_VALUE = 4;
+        this.WAITING_HEADER_VALUE_END = 5;
+        this.WAITING_HEADER_BLOCK_END = 6;
+        this.WAITING_BODY = 7;
+
+        this.current = this.WAITING_STATUS_LINE;
+        this.statusLine = '';
+        this.headers = {};
+        this.headerName = '';
+        this.headerValue = '';
+
+        this.bodyParser = null;
+    }
+
+    get isFinished() {
+        return this.bodyParser && this.bodyParser.isFinished;
+    }
+
+    get response() {
+        this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+        return {
+            statusCode: RegExp.$1,
+            statusText: RegExp.$2,
+            headers: this.headers,
+            body: this.bodyParser.content.join(''),
+        };
+    }
+
+    receive(string) {
+        for(let i = 0; i < string.length; i++) {
+            this.receiveChar(string.charAt(i));
+        }
+        // console.log(this.headers);
+    }
+
+    receiveChar(char) {
+        if (this.current === this.WAITING_STATUS_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END;
+            } else {
+                this.statusLine += char;
+            }
+        } else if (this.current === this.WAITING_STATUS_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_NAME) {
+            if (char === ':') {
+                this.current = this.WAITING_HEADER_SPACE;
+            } else if (char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END;
+            } else {
+                this.headerName += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_SPACE) {
+            if (char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE;
+            }
+        } else if (this.current === this.WAITING_HEADER_VALUE) {
+            if (char === '\r') {
+                this.current = this.WAITING_HEADER_VALUE_END;
+                this.headers[this.headerName] = this.headerValue;
+                this.headerName = '';
+                this.headerValue = '';
+            } else {
+                this.headerValue += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_VALUE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_BODY;
+                // 切换到 WAITING_BODY 状态时，根据 headers 中的 Transfer-Encoding 创建相应的bodyParser
+                if (this.headers['Transfer-Encoding'] === 'chunked') {
+                    this.bodyParser = new TrunkedBodyParser();
+                }
+            }
+        } else if (this.current === this.WAITING_BODY) {
+            // 转发给 bodyParser 进行解析
+            this.bodyParser.receiveChar(char);
+        }
+    }
+}
+
+module.exports = ResponseParser;
---
+++ week07/layout/server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+
+let receivedCount = 0;
+// Returns content-type = text/plain
+const server = http.createServer((req, res) => {
+    console.log('request received');
+    console.log(req.headers);
+
+    console.log('\n');
+    receivedCount += 1;
+    console.log(receivedCount);
+    console.log('\n');
+
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('X-Foo', 'bar');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(
+`<html maaa=a >
+<head>
+    <style>
+body div #myid{
+    width:100px;
+    background-color: #ff5000;
+}
+body div img{
+    width:30px;
+    background-color: #ff1111;
+}
+    </style>
+</head>
+<body>
+    <div>
+        <img id="myid"/>
+        <img />
+    </div>
+</body>
+</html>`);
+});
+
+server.listen(8088);
