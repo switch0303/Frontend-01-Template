d810b7552805e66dcef2e06c2c0f3c8b77f3ea50
wucong <coci315@163.com>
2020-07-12 22:27:06 +0800

    feat: 将carousel改造成组件

--- week14/component/main.js
+++ week14/component/main.js
@@ -1,104 +1,77 @@
-function create(Cls, attributes, ...children) {
-    // console.log(arguments);
-    let o;
+import {create} from "./create";
 
-    if (typeof Cls === "string") {
-        o = new Wrapper(Cls);
-    } else {
-        o = new Cls();
-    }
+// class Div {
+//     constructor(config) {
+//         // console.log("config", config);
+//         this.children = [];
+//         this.root = document.createElement("div");
+//     }
 
-    for (let name in attributes) {
-        o.setAttribute(name, attributes[name]);
-    }
+//     // set class(v) { // property
+//     //     // console.log("Parent::class", v);
+//     // }
 
-    // console.log(children);
-    for (let child of children) {
-        if (typeof child === "string")
-            child = new Text(child);
-        
-        o.children.push(child);
-    }
+//     setAttribute(name, value) { // attribute
+//         // console.log(name, value);
+//         this.root.setAttribute(name, value);
+//     }
 
-    return o;
-}
+//     appendChild(child) {
+//         this.children.push(child);
+//     }
 
-class Text {
-    constructor(text) {
-        this.root = document.createTextNode(text);
-    }
+//     mountTo(parent) {
+//         parent.appendChild(this.root);
 
-    mountTo(parent) {
-        parent.appendChild(this.root);
-    }
-}
+//         for (let child of this.children) {
+//             child.mountTo(this.root);
+//         }
+//     }
 
-class Wrapper {
-    constructor(type) {
-        // console.log("config", config);
-        this.children = [];
-        this.root = document.createElement(type);
-    }
+// }
 
-    // set class(v) { // property
-    //     // console.log("Parent::class", v);
-    // }
+// class MyComponent {
+//     constructor(config){
+//         this.children = [];
+//     }
 
-    setAttribute(name, value) { // attribute
-        // console.log(name, value);
-        this.root.setAttribute(name, value);
-    }
+//     setAttribute(name, value) { // attribute
+//         this.root.setAttribute(name, value);
+//     }
 
-    appendChild(child) {
-        this.children.push(child);
-    }
+//     appendChild(child){
+//         this.children.push(child);
+//     }
 
-    mountTo(parent) {
-        parent.appendChild(this.root);
+//     render(){
+//         return (
+//             <article>
+//                 <header>I'm a header</header>
+//                 {this.slot}
+//                 <footer>I'm a footer</footer>
+//             </article>
+//         );
+//     }
 
-        for (let child of this.children) {
-            child.mountTo(this.root);
-        }
-    }
-}
+//     mountTo(parent){
+//         this.slot = <div></div>;
 
-class Div {
-    constructor(config) {
-        // console.log("config", config);
-        this.children = [];
-        this.root = document.createElement("div");
-    }
+//         for (let child of this.children){
+//             this.slot.appendChild(child);
+//         }
 
-    // set class(v) { // property
-    //     // console.log("Parent::class", v);
-    // }
+//         this.render().mountTo(parent);
+//     }
 
-    setAttribute(name, value) { // attribute
-        // console.log(name, value);
-        this.root.setAttribute(name, value);
-    }
+// }
 
-    appendChild(child) {
-        this.children.push(child);
-    }
-
-    mountTo(parent) {
-        parent.appendChild(this.root);
-
-        for (let child of this.children) {
-            child.mountTo(this.root);
-        }
-    }
-
-}
-
-class MyComponent {
+class Carousel {
     constructor(config){
         this.children = [];
     }
 
     setAttribute(name, value) { // attribute
-        this.root.setAttribute(name, value);
+        this[name] = value;
     }
 
     appendChild(child){
@@ -106,22 +79,112 @@ class MyComponent {
     }
 
     render(){
-        return (
-            <article>
-                <header>I'm a header</header>
-                {this.slot}
-                <footer>I'm a footer</footer>
-            </article>
+
+        let children = this.data.map(url => {
+            let element = <img src={url} />;
+            element.addEventListener("dragstart", event => event.preventDefault());
+            return element;
+        });
+
+        let root = (
+            <div class="carousel">
+                {children}
+            </div>
         );
+
+        let position = 0;
+
+        let nextPic = () => {
+            let nextPosition = (position + 1) % this.data.length;
+
+            let current = children[position];
+            let next = children[nextPosition];
+
+            // 移动到起始位置时关闭动画
+            current.style.transition = "ease 0s";
+            next.style.transition = "ease 0s";
+            // 动画的起始位置
+            current.style.transform = `translateX(${-100 * position}%)`;
+            next.style.transform = `translateX(${100 - 100 * nextPosition}%)`;
+
+            // 使用setTimeout，使得动画的transition能生效
+            setTimeout(function() {
+                // 移动到终止位置时开启动画
+                current.style.transition = ""; // 置为“”来使用css中的transition
+                next.style.transition = "";
+                // 动画的终止位置
+                current.style.transform = `translateX(${-100 - 100 * position}%)`;
+                next.style.transform = `translateX(${-100 * nextPosition}%)`;
+                
+                position = nextPosition;
+                // console.log(position);
+            }, 16);
+
+            setTimeout(nextPic, 2000);
+        }
+
+        // setTimeout(nextPic, 2000);
+
+        root.addEventListener("mousedown", event => {
+            let startX = event.clientX;
+
+            let nextPosition = (position + 1) % this.data.length;
+            let lastPosition = (position - 1 + this.data.length) % this.data.length;
+
+            let current = children[position];
+            let next = children[nextPosition];
+            let last = children[lastPosition];
+
+            current.style.transition = "ease 0s";
+            next.style.transition = "ease 0s";
+            last.style.transition = "ease 0s";
+
+            current.style.transform = `translateX(${-500 * position}px)`;
+            next.style.transform = `translateX(${500 - 500 * nextPosition}px)`;
+            last.style.transform = `translateX(${-500 - 500 * lastPosition}px)`;
+            
+            let move = event => {
+                current.style.transform = `translateX(${event.clientX - startX - 500 * position}px)`;
+                next.style.transform = `translateX(${event.clientX - startX + 500 - 500 * nextPosition}px)`;
+                last.style.transform = `translateX(${event.clientX - startX - 500 - 500 * lastPosition}px)`;
+
+                // console.log(event.clientX - startX, event.clientY - startY);
+            };
+
+            let up = event => {
+                let offset = 0;
+
+                if (event.clientX - startX > 250) {
+                    offset = 1;
+                } else if (event.clientX - startX < -250) {
+                    offset = -1;
+                }
+
+                // console.log("offset", offset);
+
+                current.style.transition = ""; // 置为“”来使用css中的transition
+                next.style.transition = "";
+                last.style.transition = "";
+
+                
+                current.style.transform = `translateX(${offset * 500 - 500 * position}px)`;
+                next.style.transform = `translateX(${offset * 500 + 500 - 500 * nextPosition}px)`;
+                last.style.transform = `translateX(${offset * 500 - 500 - 500 * lastPosition}px)`;
+                
+                position = (position - offset + this.data.length) % this.data.length;
+
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+            };
+
+            document.addEventListener("mousemove", move);
+            document.addEventListener("mouseup", up);
+        });
+
+        return root;
     }
 
     mountTo(parent){
-        this.slot = <div></div>;
-
-        for (let child of this.children){
-            this.slot.appendChild(child);
-        }
-
         this.render().mountTo(parent);
     }
 
@@ -134,11 +197,14 @@ class MyComponent {
 //         <div></div>
 //     </div>
 
-let component = <MyComponent>
-    <div>text text text</div>
-</MyComponent>
+let component = <Carousel data={[
+    "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+    "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+    "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+    "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+]} />
 
-console.log(component);
+// console.log(component);
 
 component.mountTo(document.body);
 // component.setAttribute("id", "a");
