d1fa7c3d32fb0bb8f3cf4f0b7943a19c61b73372
wucong <coci315@163.com>
2020-08-18 22:23:05 +0800

    feat: publish-tool端增加打包功能，publish-server增加解压功能

--- week19/publish/publish-server-vanilla/index.js
+++ week19/publish/publish-server-vanilla/index.js
@@ -1,18 +1,23 @@
 const http = require("http");
-const fs = require("fs");
+// const fs = require("fs");
 const path = require("path");
+const unzip = require("unzipper");
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-	const matched = req.url.match(/filename=([^&]+)/);
-	const filename = matched && matched[1];
-	if (!filename) {
-		return;
-	}
+	// const matched = req.url.match(/filename=([^&]+)/);
+	// const filename = matched && matched[1];
+	// if (!filename) {
+	// 	return;
+	// }
 
-	const writeStream = fs.createWriteStream(
-		path.resolve(__dirname, "../server/public/" + filename)
-	);
+	// const writeStream = fs.createWriteStream(
+	// 	path.resolve(__dirname, "../server/public/" + filename)
+	// );
+
+	const writeStream = unzip.Extract({
+		path: path.resolve(__dirname, "../server/public/"),
+	});
 	req.pipe(writeStream);
 	req.on("end", () => {
 		res.writeHead(200, { "Content-Type": "text/plain" });
@@ -21,3 +26,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8081);
+
+console.log("Server running at http://127.0.0.1:8081/");
--- week19/publish/publish-tool/publish.js
+++ week19/publish/publish-tool/publish.js
@@ -1,41 +1,55 @@
 const http = require("http");
 // const querystring = require("querystring");
 const fs = require("fs");
+const archiver = require("archiver");
 
-let filename = "./cat.jpg";
+let packname = "./package";
 
-fs.stat(filename, (error, stat) => {
-	const options = {
-		hostname: "127.0.0.1",
-		port: 8081,
-		path: "/?filename=" + filename,
-		method: "POST",
-		headers: {
-			"Content-Type": "application/octet-stream",
-			"Content-Length": stat.size,
-		},
-	};
+// fs.stat(filename, (error, stat) => {
+const options = {
+	hostname: "127.0.0.1",
+	port: 8081,
+	path: "/?filename=" + "package.zip",
+	method: "POST",
+	headers: {
+		"Content-Type": "application/octet-stream",
+		// "Content-Length": stat.size,
+	},
+};
 
-	const req = http.request(options, (res) => {
-		console.log(`STATUS: ${res.statusCode}`);
-		console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-		// res.setEncoding("utf8");
-		// res.on("data", (chunk) => {
-		// 	console.log(`BODY: ${chunk}`);
-		// });
-		// res.on("end", () => {
-		// 	console.log("No more data in response.");
-		// });
-	});
-
-	const readStream = fs.createReadStream(filename);
-	readStream.pipe(req);
-	req.on("error", (e) => {
-		console.error(`problem with request: ${e.message}`);
-	});
-	readStream.on("end", () => {
-		// Write data to request body
-		// req.write(postData);
-		req.end();
-	});
+const req = http.request(options, (res) => {
+	console.log(`STATUS: ${res.statusCode}`);
+	console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+	// res.setEncoding("utf8");
+	// res.on("data", (chunk) => {
+	// 	console.log(`BODY: ${chunk}`);
+	// });
+	// res.on("end", () => {
+	// 	console.log("No more data in response.");
+	// });
 });
+
+req.on("error", (e) => {
+	console.error(`problem with request: ${e.message}`);
+});
+
+// const readStream = fs.createReadStream(filename);
+// readStream.pipe(req);
+const archive = archiver("zip", {
+	zlib: { level: 9 },
+});
+
+archive.directory(packname, false);
+
+// archive.pipe(fs.createWriteStream("./package.zip"));
+
+archive.finalize();
+
+archive.pipe(req);
+
+archive.on("end", () => {
+	// Write data to request body
+	// req.write(postData);
+	req.end();
+});
+// });
